83de9efb1049fd853c31d315e11fb2b4afef487a
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-17 04:54:26 -0700

    added typography component to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,39 @@ import ImageListItem from "@mui/material/ImageListItem";
 import imageHeader from "../images/mobile/imageHeader.jpg";
 import { palette } from "@mui/system";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import ImageList from "@mui/material/ImageList";
 
 const Header = () => {
   return (
-    <Box>
-      <ImageListItem>
-        <img src={imageHeader} alt="header" standard />
-      </ImageListItem>
+    <Box sx={{ backgroundColor: "#3dbefe", mt: -0.5 }}>
+      <Typography
+        sx={{
+          fontWeight: "bold",
+          pt: 2,
+          pb: 3,
+          mb: 1,
+        }}
+        variant="h3"
+        component="h1"
+        align="center"
+      >
+        We are creatives
+      </Typography>
+      <ImageList
+        sx={{ mt: -5, flexDirection: "column", objectFit: "contain" }}
+        cols={1}
+        // rowHeight={420}
+      >
+        <ImageListItem>
+          <img
+            // sx={{ height: "60vh" }}
+            src={imageHeader}
+            alt="header"
+            standard
+          />
+        </ImageListItem>
+      </ImageList>
     </Box>
   );
 };
